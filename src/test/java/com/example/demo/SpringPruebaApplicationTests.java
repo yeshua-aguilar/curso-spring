0175a6b732f91017f87628ee018650ba4ec4ad12
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-10-24 11:11:14 -0500

    mi primer commit

---
+++ src/test/java/com/example/demo/SpringPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
